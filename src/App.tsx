0845e6e5ac077a2a565163d85f9b23951782adf8
jose ortega <ortegajoseweb02mail.com>
2023-04-11 12:08:12 -0300

    Created Components[App]

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
+import Footer from './Components/Footer'
+import GamesContainer from './Components/GamesContainer'
+import Genres from './Components/Genres'
+import Header from './Components/Header'
 
 function App() {
   
 
   return (
     <div className="App">
+      <Header/>
+      <Genres/>
+      <GamesContainer/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/Components/GamesContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import GameCard from './GameCard'
+
+function GamesContainer() {
+  return (
+    <div>
+      <h1>GamesContainer</h1>
+      <GameCard/>
+    </div>
+  )
+}
+
+export default GamesContainer
