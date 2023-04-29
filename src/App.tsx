bf0395c86141708dfec6877672b8deb7ae880d07
jose ortega <ortegajoseweb02mail.com>
2023-04-28 22:21:40 -0300

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { Routes,Route } from 'react-router-dom'
 import './App.css'
 import Footer from './Components/Footer'
 import GamesContainer from './Components/GamesContainer'
 import Genres from './Components/Genres'
 import Header from './Components/Header'
+import GameDetail from './Pages/GameDetail'
+import GenresPage from './Pages/GenresPage'
+import Creators from './Pages/Creators'
+import Platforms from './Pages/Platforms'
+import Publishers from './Pages/Publishers'
+import Stores from './Pages/Stores'
+import Developers from './Pages/Developers'
 
 function App() {
   
@@ -10,8 +18,19 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      
       <Genres/>
       <GamesContainer/>
+      <Routes>
+        <Route  path='/:gameId' element={<GameDetail/>}/>
+        <Route  path='/genres' element={<GenresPage/>}/>
+        <Route  path='/creators' element={<Creators/>}/>
+        <Route  path='/platforms' element={<Platforms/>}/>
+        <Route  path='/publishers' element={<Publishers/>}/>
+        <Route  path='/stores' element={<Stores/>}/>
+        <Route  path='/developers' element={<Developers/>}/>
+        
+      </Routes>
       <Footer/>
     </div>
   )
