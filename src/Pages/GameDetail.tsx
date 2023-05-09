3232f5c196a2541ce0fd5ed4c42575936b2d7232
jose ortega <ortegajoseweb02mail.com>
2023-05-09 18:58:18 -0300

    made dinamic routingdesign details

--- src/Pages/GameDetail.tsx
+++ src/Pages/GameDetail.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { game } from '../Components/gameSample'
+
+
 
 function GameDetail() {
   return (
-    <div>GameDetail</div>
+    <>
+    <button>return</button>
+    <div>
+      <div>
+        <img src="" alt="" />
+      <h4></h4>
+      </div>
+      <h1></h1>
+      <p></p>
+    </div>
+    
+    </>
   )
 }
 
