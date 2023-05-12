8dd10c29e583d0f06af70b2f5c9fb73358b37789
jose ortega <ortegajoseweb02mail.com>
2023-05-12 19:43:09 -0300

    model the details page

--- src/styledComponents/GameDetail.ts
+++ src/styledComponents/GameDetail.ts
@@ -2,6 +2,47 @@ import styled from "styled-components"
 import { colors } from "./Colors"
 
 export const GameDetails = styled.div`
+    .text__container{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-top:20px;
+        h3{
+            display:flex;
+            gap:5px
+        }
+         #colored__text{
+          
+            color: ${colors.button3};
+        }
+    }
+
+    .button__container{
+        display: flex;
+        flex-direction: row;
+        justify-content:space-around;
+        margin-top:20px;
+        margin-bottom: 20px;
+    }
+    .map__container{
+        width:90%;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        padding: 10px;
+        margin-left: auto;
+        margin-right: auto;
+        gap: 10px;
+        margin-top:20px;
+        h4{
+            background-color: ${colors.button3};
+            border-radius:10px;
+            padding: 5px;
+            color: ${colors.primary}
+        }
+    }
+
     .principal__container{
         text-align: center;
         div{
