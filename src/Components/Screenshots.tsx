f870ffcfde25a0524b40f62a15a4c8f7a20b2de7
jose ortega <ortegajoseweb02mail.com>
2023-05-15 19:06:45 -0300

    screenshots slider

--- src/Components/Screenshots.tsx
+++ src/Components/Screenshots.tsx
@@ -1,15 +1,29 @@
-import React from 'react'
-import { fetchData } from '../Hooks/renderAsFetch'
- 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, A11y,EffectCreative } from 'swiper';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import 'swiper/css';
+
 function Screenshots(props:{screenshots:any}) {
    
     const data = props.screenshots.read()
-  return (
-    <>
+  return (<div style={{marginTop: '20px',marginBottom: '20px'}}>
+  <h3>Screnshots:</h3>
+    <Swiper spaceBetween={10}
+    modules={[Navigation, Pagination, A11y,EffectCreative]}
+    navigation
+      pagination={{ clickable: true }}
+      
+    slidesPerView={1}>
         {data?.results.map((r:any) => (
-            <img src={r.image} style={{width: '30%'}} alt="" key={r.id}/>
+          <SwiperSlide>
+            <img src={r.image} style={{width:"100%"}}  alt="" key={r.id}/>
+            </SwiperSlide>
         ))}
-        Screenshots</>
+        
+        </Swiper>
+        </div>
   )
 }
 
